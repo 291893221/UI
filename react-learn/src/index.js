e003e95a038188cd9e244e881159724f24fe20e3
gitadmin <752802762@qq.com>
2020-06-05 11:48:51 +0800

    enter

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TodoList from './component/TodoList';
+import HelloWorld from './page/helloWorld/HelloWorld'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoList/>
+    <HelloWorld/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ react-learn/src/page/helloWorld/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HelloWorld.scss'
+
+class HelloWorld extends React.Component{
+    render(){
+        return <div><h1>Hello World</h1></div>
+    }
+}
+export default HelloWorld
