7249301d6bb30df45681a38502933624aab456a5
291893221@qq.com <291893221@qq.com>
2020-07-09 16:36:21 +0800

    table

---
+++ react-learn/src/pages/table/Table.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Table(){
+    return <div>表格</div>
+}
+export default Table;
--- react-learn/src/routers/Router.js
+++ react-learn/src/routers/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter as RouterRRD, Route, Switch } from 'react-router-dom'
 import Default from '../pages/default/Default';
 import Login from '../pages/login/Login';
 import Main from '../pages/main/Main';
+import Table from '../pages/table/Table';
 function Router() {
     return <RouterRRD>
         <Switch>
             <Route exact path='/' component={Default}></Route>
             <Route path='/login' component={Login}></Route>
             <Route path='/main' component={Main}></Route>
+            <Route path='/table' component={Table}></Route>
         </Switch>
     </RouterRRD>
 }
